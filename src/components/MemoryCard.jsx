f3ea7a8901581e4078a44f16b9eed9a3be7d1f97
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-03-25 12:04:07 -0400

    Updates to MemoryCard CSS and JSX files for step 2

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,13 +3,32 @@ import './MemoryCard.css';
 import image from './digitalcrafts-logo-white-y.png';
 
 class MemoryCard extends Component {
+    state = { isFlipped: false }
+
+    clickHandler = () => {
+        this.setState( { isFlipped: !this.state.isFlipped });
+    }
   render() {
-    return (
-        <div className="MemoryCard">
-            <img src= { image } alt="Digital Crafts logo" />
+      const { isFlipped } = this.state;
+
+      const memoryCardInnerClass = 
+      "MemoryCardInner" + (isFlipped === true ? " flipped" : "")
+
+    return ( 
+
+        <div className="MemoryCard" onClick= { this.clickHandler.bind(this) }>
+            <div className= { memoryCardInnerClass }>
+                <div className="MemoryCardBack">
+                    <img src= { image } alt="Digital Crafts logo" />
+                </div>
+                <div className="MemoryCardFront">
+                    ∆
+                </div>
+            </div>
         </div>
-    );
-  }
+        );
+    }
 }
+
 
 export default MemoryCard;
