1e7cf2029b50d7741abba7941e1b4b4532bcc803
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-03-26 12:06:53 -0400

    Adding functions to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 import MemoryCard from './components/MemoryCard';
 
-function App() {
+const generateDeck = () => {
+  const symbols = [`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`];
+  const deck = [];
+    for (let i = 0; i < 16; i++) {
+      deck.push({
+        symbol: symbols[i % 8],
+        isFlipped: false
+      });
+    shuffle(deck);
+    return deck;
+    }
+}
+function shuffle(array) {
+  const newArray = array.concat();
+  newArray.sort(() => 0.5 - Math.random());
+  return newArray;
+}
+
+class App extends Component {
+    state = { 
+      deck: generateDeck(),
+      pickedCards: [] 
+    }
+  render() {
+   const cardsJSX = this.state.deck.map((card, index) => {
+     return  <MemoryCard />;
+    }
+   );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,31 +38,20 @@ function App() {
       <h3>Match cards to win</h3>
       </header>
       <div>
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
+      {cardsJSX.slice(0,4)}
       </div>
       <div>
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
+      {cardsJSX.slice(4,8)}
       </div>
       <div>
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
+      {cardsJSX.slice(8,12)}
       </div>
       <div>
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
+      {cardsJSX.slice(12,16)}
       </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
